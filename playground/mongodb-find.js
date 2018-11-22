a47cc3883fc1df39affde8ffaf3fbea4b352ed34
Konstantin Dinev <konstantin.dinev@atos.net>
2018-11-22 13:46:13 +0200

    init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,23 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Todos').find({
+    _id: new ObjectID('5bf57d11069f7b0c4bef1ca9')
+  }).toArray().then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+  //db.close();
+});
