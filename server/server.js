9655bf76b9b047d0d3cf61780bb64dbff747ff8e
Konstantin Dinev <konstDinev@gmail.com>
2018-12-05 17:31:50 +0200

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 const _ = require('lodash');
 
@@ -117,6 +118,10 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
