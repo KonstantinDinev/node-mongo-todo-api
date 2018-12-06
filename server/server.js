0d42209eaa677873caee2955e9192b006608f557
Konstantin Dinev <konstDinev@gmail.com>
2018-12-06 15:41:35 +0200

    add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -124,6 +124,20 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('xauth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`Started up port ${port}`);
 });
